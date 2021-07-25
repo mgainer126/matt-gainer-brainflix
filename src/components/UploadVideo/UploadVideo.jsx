be59759a060f0390ea0ebb10a2628af3cd7d4d89
Matt Gainer <matt.gainer126@hotmail.com>
2021-07-25 09:12:02 -0400

    updates to the sytling of the upload page

--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -4,7 +4,6 @@ import thumbnail from "../../assets/images/Upload-video-preview.jpg";
 function UploadVideo() {
   return (
     <thumbnail className="thumbnail">
-      {/* <h1 className="thumbnail__heading">Upload Video</h1> */}
       <h4 className="thumbnail__video-title">VIDEO THUMBNAIL</h4>
       <img
         className="thumbnail__img"
